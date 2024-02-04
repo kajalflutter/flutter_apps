f91ce61e753032415f18dac0e31c7548073b4f70
kajalflutter <kajalflutter@gmail.com>
2024-02-04 18:24:12 +0530

    Stateful Assignment 2
    Stateful Assignment 2

---
+++ stateful_assignment2/lib/assignment2.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+class Assignment2 extends StatefulWidget {
+  const Assignment2({super.key});
+  @override
+  State<Assignment2> createState() => _Assignment2State();
+}
+
+class _Assignment2State extends State<Assignment2> {
+  bool box1Color = false;
+  bool box2Color = false;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Colors Box"),
+          backgroundColor: Colors.blue,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: box1Color ? Colors.red : Colors.black,
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          box1Color = !box1Color;
+                        });
+                      },
+                      child: Text("Color Box1"),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                Column(
+                  children: [
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: box2Color ? Colors.blue : Colors.black,
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          box2Color = !box2Color;
+                        });
+                      },
+                      child: Text("Color Box2"),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
