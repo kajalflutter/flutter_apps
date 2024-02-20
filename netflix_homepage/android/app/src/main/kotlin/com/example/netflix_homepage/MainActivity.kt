ab5846dc7f2bc0b8671c6d2ce70d1a83f4bd9902
kajalflutter <156578934+kajalflutter@users.noreply.github.com>
2024-02-20 09:58:26 +0530

    netflix_homepage

---
+++ netflix_homepage/android/app/src/main/kotlin/com/example/netflix_homepage/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.netflix_homepage
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ netflix_homepage/lib/assignment10.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+
+class Assignment10 extends StatelessWidget {
+  const Assignment10({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("NETFLIX"),
+        backgroundColor: Colors.red,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+            const Text(
+              "MOVIES",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Image.network(
+                      height: 400,
+                      width: 270,
+                      "https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC/et00311762-lmdexnggxy-portrait.jpg"),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Image.network(
+                    "https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC/et00311762-lmdexnggxy-portrait.jpg",
+                    height: 400,
+                    width: 270,
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Image.network(
+                    "https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC/et00311762-lmdexnggxy-portrait.jpg",
+                    height: 400,
+                    width: 270,
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            const Text(
+              "WEB SERIES",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Image.network(
+                    "https://assetscdn1.paytm.com/images/catalog/product/H/HO/HOMSHERLOCK-HOLHK-P63024784A1CC1B/1563111214645_0..jpg",
+                    height: 200,
+                    width: 150,
+                    fit: BoxFit.cover,
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Image.network(
+                    "https://dnm.nflximg.net/api/v6/2DuQlx0fM4wd1nzqm5BFBi6ILa8/AAAAQeIeKt7LlqIJPKrT4aQijclj7K43xRSU3dQXNESNdNbnnJbT6LLWVRT9srUUbHbOo-iOH-8v3o16pUDMQ6tCgNGlkvfwvDOprROIZpQ2rgHtop9rHvbYlvzavMmUSGBCXjynJ80dn4nqZzZmzIUJMQpS.jpg?r=943",
+                    height: 200,
+                    width: 150,
+                    fit: BoxFit.cover,
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Image.network(
+                    "https://www.tallengestore.com/cdn/shop/products/PeakyBlinders-NetflixTVShow-ArtPoster_125897c4-6348-41e8-b195-d203700ebcca.jpg?v=1619864555",
+                    height: 200,
+                    width: 150,
+                    fit: BoxFit.cover,
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              "MOST POPULAR",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Image.network(
+                    "https://assetscdn1.paytm.com/images/catalog/product/H/HO/HOMSHERLOCK-HOLHK-P63024784A1CC1B/1563111214645_0..jpg",
+                    height: 200,
+                    width: 150,
+                    fit: BoxFit.cover,
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Image.network(
+                    "https://dnm.nflximg.net/api/v6/2DuQlx0fM4wd1nzqm5BFBi6ILa8/AAAAQeIeKt7LlqIJPKrT4aQijclj7K43xRSU3dQXNESNdNbnnJbT6LLWVRT9srUUbHbOo-iOH-8v3o16pUDMQ6tCgNGlkvfwvDOprROIZpQ2rgHtop9rHvbYlvzavMmUSGBCXjynJ80dn4nqZzZmzIUJMQpS.jpg?r=943",
+                    height: 200,
+                    width: 150,
+                    fit: BoxFit.cover,
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Image.network(
+                    "https://www.tallengestore.com/cdn/shop/products/PeakyBlinders-NetflixTVShow-ArtPoster_125897c4-6348-41e8-b195-d203700ebcca.jpg?v=1619864555",
+                    height: 200,
+                    width: 150,
+                    fit: BoxFit.cover,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
