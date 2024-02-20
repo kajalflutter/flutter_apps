6f3ca689009450a9e0dd5fbf1abe84f65e07557b
kajalflutter <156578934+kajalflutter@users.noreply.github.com>
2024-02-20 10:07:13 +0530

    netflix_homepage

--- netflix_homepage/lib/assignment10.dart
+++ netflix_homepage/lib/assignment10.dart
@@ -113,32 +113,29 @@ class Assignment10 extends StatelessWidget {
                     width: 20,
                   ),
                   Image.network(
-                    "https://assetscdn1.paytm.com/images/catalog/product/H/HO/HOMSHERLOCK-HOLHK-P63024784A1CC1B/1563111214645_0..jpg",
-                    height: 200,
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0kR0gMemRl9ylPTzmmuQQVb10vo8n7kXL7BeHkeo_4lmJS56C8-WKIy_GYK12wnEmPlc",
+                    height: 190,
                     width: 150,
-                    fit: BoxFit.cover,
                   ),
                   const SizedBox(
-                    width: 20,
+                    width: 10,
                   ),
                   Image.network(
-                    "https://dnm.nflximg.net/api/v6/2DuQlx0fM4wd1nzqm5BFBi6ILa8/AAAAQeIeKt7LlqIJPKrT4aQijclj7K43xRSU3dQXNESNdNbnnJbT6LLWVRT9srUUbHbOo-iOH-8v3o16pUDMQ6tCgNGlkvfwvDOprROIZpQ2rgHtop9rHvbYlvzavMmUSGBCXjynJ80dn4nqZzZmzIUJMQpS.jpg?r=943",
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZ5Cq8kozpWIaq5Aohw4rjKkh_eE7nUkDV5zcHClQaYw&s",
                     height: 200,
                     width: 150,
-                    fit: BoxFit.cover,
-                  ),
-                  const SizedBox(
-                    width: 20,
                   ),
                   Image.network(
-                    "https://www.tallengestore.com/cdn/shop/products/PeakyBlinders-NetflixTVShow-ArtPoster_125897c4-6348-41e8-b195-d203700ebcca.jpg?v=1619864555",
-                    height: 200,
+                    "https://dbdzm869oupei.cloudfront.net/img/posters/preview/91008.png",
+                    height: 190,
                     width: 150,
-                    fit: BoxFit.cover,
                   ),
                 ],
               ),
             ),
+            const SizedBox(
+              height: 40,
+            )
           ],
         ),
       ),
